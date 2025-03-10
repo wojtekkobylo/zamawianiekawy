829efd393012f816d2d668cb52d4819986c93b84
Maks <dawidjasperwojcik@gmail.com>
2025-03-10 11:04:47 +0100

    dzialanie kotlin

--- app/src/main/java/com/example/zamawianiekawy/MainActivity.kt
+++ app/src/main/java/com/example/zamawianiekawy/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.zamawianiekawy
 
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,8 +25,39 @@ class MainActivity : AppCompatActivity() {
         val radioGroup = findViewById<RadioGroup>(R.id.kawyradio)
         radioGroup.setOnCheckedChangeListener { _, checkedId ->
             val wybor = findViewById<RadioButton>(checkedId).text
-
         }
 
+            val cukierciukbox = findViewById<CheckBox>(R.id.cukierciukbox)
+            val mlekociukbox = findViewById<CheckBox>(R.id.mlekociukbox)
+
+        cukierciukbox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val cukier = true;
+            } else {
+                val cukier = false
+            }
+        }
+        mlekociukbox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val mleko = true;
+            } else {
+                val mleko = false;
+            }
+        }
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val ilosckawy = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(this@MainActivity, "awaw", Toast.LENGTH_SHORT)
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(this@MainActivity, "bwbw", Toast.LENGTH_SHORT)
+            }
+
+        })
     }
 }
